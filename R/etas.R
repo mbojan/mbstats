8225fd81513571fcb725f410c3086a0df7e34843
Michal Bojanowski <michal2992@gmail.com>
2021-03-14 19:55:21 +0100

    Update man page of etas()

--- R/etas.R
+++ R/etas.R
@@ -1,60 +1,44 @@
-#' Calculate Eta coefficients
+#' Eta coefficients
 #'
-#' This generic function calculates Eta coefficients which are also known as
-#' "Correlation ratios" or (the squared value) as the "Proportion of
-#' explained variance".
-#'
-#' The Eta coefficient (more specifically its squared value) has a
-#' interpretation in terms of the proportion of explained variance.
-#'
-#' In the decision theory the interpretation is related to the identification
-#' problem that involves two variables: \eqn{y} and \eqn{x}. The task is two
-#' identify the values of \code{y}.
-#'
+#' Calculate Eta coefficients, known as "Correlation ratios". Squared value of
+#' Eta has an interpretation in terms of the proportion of explained variance.
+#' The interpretation follows from a problem of predicting values of \eqn{y}.
 #' The value of the \eqn{\eta^2}{Eta^2} is the proportion by which the error of
-#' predicting values of \eqn{y} is reduced by using the information contained
-#' in \eqn{x}.
+#' predicting values of \eqn{y} is reduced by using the information provided by
+#' \eqn{x}.
 #'
 #' @return Values of eta and partial eta coefficients.
 #'
 #' @param object the R object
-#' @param fac vector for conditioning variable
 #' @param ... arguments passed to other methods
 #'
 #' @export
-#'
-#' @examples
-#' x1 <- rnorm(50)
-#' x2 <- rnorm(50)
-#' y <- 5 + 2*x1 + rnorm(50,0,2) + 3*x2 + rnorm(50,0,.5)
-#'
-#' # method for numeric
-#' etas( y, rep(1:2, each=25) )
-#'
-#' # method for 'lm' which calls 'anova'
-#' m <- lm( y ~ x1 + x2 )
-#' etas(m)
 
 etas <- function( object, ... ) UseMethod("etas")
 
 
 
-
-
-#' @details For numeric vectors the function requires additional argument: a
-#' vector of the same length as the first.  The result is a value of the
-#' \eqn{\eta^2}{Eta^2} assuming that we want to predict the values of
-#' \code{object} with the values of \code{fac} using the so called ``Type I
-#' regression of means''.
+#' @describeIn etas The default method expect vectors. The function requires
+#'   additional argument `fac` -- a vector of the same length as `object`. The
+#'   result is a value of the \eqn{\eta^2}{Eta^2} assuming that we want to
+#'   predict the values of `object` with the values of `fac` using the so called
+#'   "Type I regression of means". For two variables \eqn{y} and \eqn{x} the
+#'   \eqn{\eta}{Eta} is given by the formula:
 #'
-#' For two variables \eqn{y} and \eqn{x} the \eqn{\eta}{Eta} is given by the
-#' formula:
+#'   \deqn{\eta^2 = ( D^2(y) - E[D^2(y|x)] ) / D^2(y)}{Eta^2 = ( D^2(y) - E[D^2(y|x)] ) / D^2(y)}
 #'
-#' \deqn{\eta^2 = ( D^2(y) - E[D^2(y|x)] ) / D^2(y)}{Eta^2 = ( D^2(y) - E[D^2(y|x)] ) / D^2(y)}
+#' @param fac vector for conditioning variable
 #'
-#' @method etas default
 #' @export
-#' @rdname etas
+#'
+#' @examples
+#' ### Generate some data
+#' x1 <- rnorm(50)
+#' x2 <- rnorm(50)
+#' y <- 5 + 2*x1 + rnorm(50,0,2) + 3*x2 + rnorm(50,0,.5)
+#'
+#' ### Method for vectors
+#' etas( y, rep(1:2, each=25) )
 etas.default <- function( object, fac, ... ) {
 	n <- length(object)
 	if( length(fac) != n )
@@ -64,21 +48,19 @@ etas.default <- function( object, fac, ... ) {
 
 
 
-#' @details For objects of class \code{anova} the function calculates the Eta's
-#' and Partial Eta Squares for all effects in the given model. In this setting
-#' the eta squares for the given effect are equal to:
+#' @describeIn etas For objects of class `anova` the function calculates the
+#'   Eta's and Partial Eta Squares for all effects in the given model. In this
+#'   setting the eta squares for the given effect are equal to:
 #'
-#' \deqn{\frac{SS_{effect}}{SS_{total}}}{SSeffect / SStotal}
+#'   \deqn{\frac{SS_{effect}}{SS_{total}}}{SSeffect / SStotal}
 #'
-#' where \eqn{SS} are apropriate Sums of Squares.  The ``Partial Eta Squares''
-#' for the given effect are equal to:
+#'   where \eqn{SS} are apropriate Sums of Squares.  The ``Partial Eta Squares''
+#'   for the given effect are equal to:
 #'
-#' \deqn{\frac{SS_{effect}}{SS_{effect}+SS_{resid}}}{SSeffect / (SSeffect+SSresid)}
+#'   \deqn{\frac{SS_{effect}}{SS_{effect}+SS_{resid}}}{SSeffect / (SSeffect+SSresid)}
 #'
-#' @method etas anova
 #' @export
-#' @rdname etas
-etas.anova <- function( object, ... ) {
+etas.anova <- function(object, ...) {
 	nam <- row.names(object["Sum Sq"])
 	ss <- object[["Sum Sq"]]
 	sstotal <- sum(ss)
@@ -92,10 +74,15 @@ etas.anova <- function( object, ... ) {
 
 
 
-#' @details For objects of class \code{lm} the function is applied on the
-#' result of calling \code{anova}.
+#' @describeIn etas For objects of class `lm` the function is applied on the
+#' result of calling [anova()].
 #'
-#' @method etas lm
 #' @export
-#' @rdname etas
-etas.lm <- function( object, ... ) etas( stats::anova(object) )
+#'
+#' @examples
+#'
+#' ### Method for 'lm' which calls 'anova'
+#' m <- lm( y ~ x1 + x2 )
+#' etas(m)
+
+etas.lm <- function(object, ...) etas.anova(stats::anova(object))
