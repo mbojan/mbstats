fdf4d82d25b11a22b2f01288e522b7a5595ceee4
Michal Bojanowski <michal2992@gmail.com>
2021-03-14 22:49:56 +0100

    Update man of anova.princomp()

--- R/anova.princomp.R
+++ R/anova.princomp.R
@@ -3,25 +3,26 @@
 #' Calculate percent of explained variance for Principal Component Analysis
 #' objects.
 #'
-#' The function calculates percentages of variance explained for each component
-#' resulting from Principal Component Analysis. The variance is equal to:
-#' \deqn{100\frac{\sigma_i^2}{\sum_i \sigma_i^2}}{sigma^2 / sum( sigma^2 ) *
-#' 100} where \eqn{\sigma}{sigma} is equal to component's standard deviation.
-#'
-#' @param object object of class \code{princomp}
+#' @param object object of class `princomp`
 #' @param ... other arguments currently not supported
 #'
-#' @return A vector of length equal to a number of components existing in
-#' \code{object}. Each element containes the percent of variance explained by
-#' the corresponding component.
+#' @details The function calculates percentages of variance explained for each
+#'   component resulting from Principal Component Analysis. The variance is
+#'   equal to: \deqn{100\frac{\sigma_i^2}{\sum_i \sigma_i^2}}{sigma^2 / sum(
+#'   sigma^2 ) * 100} where \eqn{\sigma}{sigma} is equal to component's standard
+#'   deviation.
 #'
-#' @seealso \code{\link[stats]{princomp}}, \code{\link[stats]{anova}}
+#' @return A vector of length equal to a number of components existing in
+#'   `object`. Each element contains the percent of variance explained by
+#'   the corresponding component.
+#'
+#' @seealso [stats::princomp()], [stats::anova()]
 #'
 #' @export
 #' @method anova princomp
 #'
 #' @examples
-#' pcmodel <- princomp( USArrests, cor=TRUE )
+#' pcmodel <- princomp(USArrests, cor=TRUE)
 #' pcmodel
 #' anova(pcmodel) # variance prc.
 
