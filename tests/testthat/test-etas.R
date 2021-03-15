55404b9e5644e9312e75df3dce33a7d1b50e43c5
Michal Bojanowski <michal2992@gmail.com>
2021-03-15 17:09:23 +0100

    More tests of etas()

--- tests/testthat/test-etas.R
+++ tests/testthat/test-etas.R
@@ -1,22 +1,31 @@
-d <- structure(
-  list(
-    x = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3,
-          3, 3, 3, 3, 3, 3, 3),
-    y = c(12, 12, 14, 16, 17, 17, 18, 18, 20, 23, 10, 11, 11, 11, 12, 13, 14,
-          14, 15, 18, 8, 8, 8, 9, 10, 10, 11, 13, 14, 15)
-  ),
-  class = "data.frame"
+d <- data.frame(
+  x = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3,
+        3, 3, 3, 3, 3, 3, 3),
+  y = c(12, 12, 14, 16, 17, 17, 18, 18, 20, 23, 10, 11, 11, 11, 12, 13, 14,
+        14, 15, 18, 8, 8, 8, 9, 10, 10, 11, 13, 14, 15)
 )
 
-test_that("eta comes out as in Excel :)", {
-  expect_equal(
-    with(d, etas(y, x, pop_var = TRUE)),
-    0.463831867057674
+test_that("default method just works", {
+  expect_silent(
+    with(d, etas(y, x, pop_var = TRUE))
+  )
+  expect_silent(
+    with(d, etas(y, x, pop_var = FALSE))
   )
 })
 
 test_that("lm method just works", {
   expect_silent(
-    r <- etas(lm(y ~ x, data = d))
+    etas(lm(y ~ x, data = transform(d, x = as.character(x))))
   )
 })
+
+test_that("eta comes out as in Excel :)", {
+  expect_equal(
+    r1 <- with(d, etas(y, x, pop_var = TRUE)),
+    0.463831867057674
+  )
+  r2 <- etas(lm(y ~ x, data = transform(d, x = as.character(x))))
+  expect_equal(r1, r2[1,1])
+})
+
